e85003d461affa212d3cf2cd03e93f5552b63f6a
Michael <mwcheeselover@gmail.com>
2023-09-27 14:39:07 -0400

    Finish styling in style.css

--- calc.js
+++ calc.js
@@ -11,5 +11,30 @@ function multiply(a,b) {
 }
 
 function divide(a,b) {
+    if(b == 0) return "Can't divide by zero!";
     return a / b;
 }
+
+function operate(a,btn-operator, b)
+{
+    if (btn-operator === "+")
+    {
+        return add(a,b);
+    }
+
+    if(btn-operator === "-")
+    {
+        return subtract(a,b);
+    }
+
+    if (btn-operator === "*") {
+        return multiply(a,b);
+    }
+
+    if (btn-operator === "/") {
+        return divide(a,b);
+    }
+}
+let a = 0;
+let btn-operator = "";
+let b = 0;
